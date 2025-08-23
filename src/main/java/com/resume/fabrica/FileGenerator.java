90c219543f7ba84e1a1da5bccffd98dd1a462236
ailapina150 <alapina840@gmail.com>
2025-08-23 19:18:18 +0300

    fabrica

--- src/main/java/com/resume/fabrica/FileGenerator.java
+++ src/main/java/com/resume/fabrica/FileGenerator.java
@@ -3,6 +3,7 @@ package com.resume.fabrica;
 import com.resume.model.Employee;
 
 public interface FileGenerator {
-    public final String FOLDER_PATH = "c:\\Users\\PC\\Downloads\\";
+    String FOLDER_PATH = "c:\\Users\\PC\\Downloads\\";
+
     String generateFile(Employee employee);
 }
--- src/test/java/com/resume/services/ProjectServicesTest.java
+++ src/test/java/com/resume/services/ProjectServicesTest.java
@@ -1,9 +1,6 @@
 package com.resume.services;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ProjectServicesTest {
 
